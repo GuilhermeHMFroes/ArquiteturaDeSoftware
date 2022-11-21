8127137c87856d5158c55468d492da5fe82b31ef
GuilhermeFroes <guil.henrique.fr@gmail.com>
2022-11-21 08:20:47 -0300

    Atividade 1 v1

---
+++ Atividade 1/main.py
@@ -0,0 +1,10 @@
+'''
+
+Forme uma dupla e pesquise sobre o padrão de arquitetura de software em 3 camadas para a tecnologia que vocês tem
+mais afinidade. Implemente um exemplo simples e apresente para a turma.
+
+'''
+
+if '__main__':
+
+    print('Hello world!')
